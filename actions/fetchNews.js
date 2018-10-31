f751dc70bed44b4719a43ca7ec24db480d584484
Eyþór Freyr Óskarsson <eythoro16@ru.is>
2018-10-31 08:45:44 +0000

    eslint activated

--- actions/fetchNews.js
+++ actions/fetchNews.js
@@ -1,18 +1,5 @@
 import { FETCHING_NEWS, FETCHING_NEWS_FAILURE, FETCHING_NEWS_SUCCESS } from '../constants/Reducers';
 
-export function fetchNews() {
-  return (dispatch) => {
-    dispatch(getNews());
-    return fetch('http://hn.algolia.com/api/v1/search?tags=front_page')
-      .then(res => res.json())
-      .then((data) => {
-        console.log(data);
-        dispatch(getNewsSuccess(data));
-      })
-      .catch(err => dispatch(getNewsFailure(err)));
-  };
-}
-
 function getNews() {
   return {
     type: FETCHING_NEWS,
@@ -29,5 +16,17 @@ function getNewsSuccess(data) {
 function getNewsFailure() {
   return {
     type: FETCHING_NEWS_FAILURE,
+  };
+}
+
+export default function fetchNews() {
+  return (dispatch) => {
+    dispatch(getNews());
+    return fetch('http://hn.algolia.com/api/v1/search?tags=front_page')
+      .then(res => res.json())
+      .then((data) => {
+        dispatch(getNewsSuccess(data));
+      })
+      .catch(err => dispatch(getNewsFailure(err)));
   };
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,62 +1,47 @@
 import React from 'react';
 import {
-  StyleSheet, Text, TouchableOpacity, View, ActivityIndicator,
+  StyleSheet, Text, View, ActivityIndicator,
 } from 'react-native';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { fetchNews } from '../actions/fetchNews';
+import fetchNews from '../actions/fetchNews';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
 class HomeScreen extends React.Component {
-	static navigationOptions = {
-	  title: 'Hackernews',
-	  headerStyle: {
-	    backgroundColor: '#ff6600',
-	  },
-	};
+  static navigationOptions = {
+    title: 'Hackernews',
+    headerStyle: {
+      backgroundColor: '#ff6600',
+    },
+  };
 
-	componentDidMount() {
-	  this.props.fetchNews();
-	}
+  componentDidMount() {
+    this.props.fetchNews();
+  }
 
-	render() {
-	  const { news, isFetching } = this.props.news;
-	  console.log(news);
+  render() {
+    const { news, isFetching } = this.props.news;
+    console.log(news);
 
-	  if (isFetching) {
-	    return (
-  <View
-    style={{
-					  flex: 1,
-					  flexDirection: 'column',
-					  alignItems: 'center',
-					  justifyContent: 'center',
-    }}
-  >
-    <ActivityIndicator size="large" />
-  </View>
-	    );
-	  }
-	  const titles = news.hits.map(item => <Text key={item.objectID}>{item.title}</Text>);
-	  return (
-  <View
-    style={{
-				  flex: 1,
-				  flexDirection: 'column',
-				  alignItems: 'center',
-				  justifyContent: 'center',
-    }}
-  >
-    {titles}
-  </View>
-	  );
-	}
+    if (isFetching) {
+      return (
+        <View style={styles.container}>
+          <ActivityIndicator size="large" />
+        </View>
+      );
+    }
+    const titles = news.hits.map(item => <Text key={item.objectID}>{item.title}</Text>);
+    return <View style={styles.container}>{titles}</View>;
+  }
 }
 
 function mapStateToProps(state) {
